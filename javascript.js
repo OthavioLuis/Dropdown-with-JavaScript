1ab8b68ec15b339c09f9f5158e543a0f4471c304
OthavioLuis] <othaviorodriguesf@gmail.com>
2024-01-31 20:52:49 -0300

    Remoção de linhas inúteis.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,5 @@
 let icon = document.querySelector('.lista-li-1')
 let icon2 = document.querySelector('.lista-li-2')
-let img1 = document.getElementById('seta1')
-let img2 = document.getElementById('seta2')
 
 function mostrar2() {
     icon.style.display = 'none'
